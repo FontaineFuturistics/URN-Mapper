4e7b7814d434a07c6147f13bec393d3440fa3019
Liam Estell <76744930+FontaineFuturistics@users.noreply.github.com>
2022-05-26 20:07:07 -0400

    Attempted to implement str_diff search
    There are still some bugs that need sorting out

--- omni1.js
+++ omni1.js
@@ -4,10 +4,18 @@ let http = require("http");
 // Import mappings
 let mappings = require("./mappings.json") 
 
+// Get mapping keys
+let map_keys = Object.keys(mappings)
+
+// Set diff tolerance fraction
+const diff_tol = 0.25
+
 //TODO: I'd like to be able to change mappings dynamically during runtime
 
 // Create the HTTP server
 http.createServer(function (req, res) {
+
+    console.log("Request received")
 
     let req_str = req.url.split('=', 2)[1] // Split the url on the equals
 
@@ -21,15 +29,39 @@ http.createServer(function (req, res) {
     // If that didn't work, move to plan B
     if (map_url == undefined) {
 
-        console.log(str_diff(key, "test"))
+
+        console.log("Plan B")
+        map_url = []
+
+        console.log(map_keys.length)
+
+        // Run against each key 
+        for (i = 0; i < map_keys.length; i++) {
+
+            console.log("Iteration" + i)
+
+            // Get current key
+            let ckey = map_keys[i]
+
+            // Calculate maximum allowable difference
+            let max_diff = Math.floor(diff_tol * ckey.length)
+
+            // See if diff is low enough
+            if (str_diff(key, ckey) <= max_diff) {
+                map_url.push(mappings[ckey])
+            } // End of output add
+
+        } // End of each key loop
 
     } // End of diff system loop
 
-    
+    console.log("progress")
+    res.write("output: " + map_url);
+    res.end();
     
 }).listen(8080);
 
-// str_diff function
+// str_diff function to determine the difference between two strings
 function str_diff(base, ref) {
 
     // Initialize arrays
@@ -40,10 +72,10 @@ function str_diff(base, ref) {
     let diff = 0
 
     // For each index in the ref_array
-    for (i = 0; i < ref_array.length; i++) {
+    for (k = 0; k < ref_array.length; k++) {
 
         // If the arrays are not equal at the given index increase the diff
-        if (base_array[i] != ref_array[i]) {
+        if (base_array[k] != ref_array[k]) {
             diff++
         } // End of diff++ gate
 
